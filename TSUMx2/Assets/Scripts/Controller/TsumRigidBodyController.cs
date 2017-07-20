5e6ddfd8080ef35b31a6e54e28e66231f0bc4ff9
Yugo KAJIWARA <y.kaji.oita@gmail.com>
2017-07-20 15:27:13 +0900

    Implement add and dispose TSUM function

---
+++ TSUMx2/Assets/Scripts/Controller/TsumRigidBodyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsumRigidBodyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.angularVelocity = 9.8f * 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
