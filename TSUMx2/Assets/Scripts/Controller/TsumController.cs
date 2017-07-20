6ce45a3ab4a530c47b7272aef14581f2ddf24ca6
Yugo KAJIWARA <y.kaji.oita@gmail.com>
2017-07-20 13:12:02 +0900

    Add Tsum

---
+++ TSUMx2/Assets/Scripts/Controller/TsumController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsumController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _tsumPrefab;
+
+    [SerializeField]
+    private Sprite[] _tsumSprites;
+
+    const float StartHeight = 7.0f;
+
+    [SerializeField]
+    public int DropTsumCnt = 55;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DropTsum(DropTsumCnt));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     IEnumerator DropTsum(int cnt) {
+        for(int i = 0; i < cnt; i++) {
+            var tsum = Instantiate(_tsumPrefab);
+            tsum.transform.position = new Vector3(
+                Random.Range(-2.0f, 2.0f),
+                StartHeight,
+                Random.Range(-40, 40));
+
+            var tsumTexture = tsum.GetComponent<SpriteRenderer>();
+            tsumTexture.sprite = _tsumSprites[Random.Range(0, _tsumSprites.Length)];
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
